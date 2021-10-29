3911c047f3a6eb96e6f3394e578d8a7c94590ae9
Trevor Sears <trevor@trevorsears.com>
2021-10-29 14:01:44 -0400

    Added `/live-templates/javascript/impo.js`.

---
+++ live-templates/javascript/impo.js
@@ -0,0 +1,18 @@
+import { $SYMBOLS$ } from "$FILE_LOCATION$";$END$
+
+/* Category:     JavaScript
+ * Abbreviation: impo
+ * Description:  Inserts an `import { x } from "y";` statement.
+ * 
+ * Template Variable Settings:
+ * 
+ *   $FILE_LOCATION$
+ *     Expression: <none>
+ *     Default value: <none>
+ *     Skip is defined: NO
+ * 
+ *   $SYMBOLS$
+ *     Expression: completeSmart()
+ *     Default value: <none>
+ *     Skip is defined: NO
+ */
