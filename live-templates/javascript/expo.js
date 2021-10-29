ef3ff63c9a0f5409d22bd46600d3df8a76776edf
Trevor Sears <trevor@trevorsears.com>
2021-10-29 14:00:58 -0400

    Added `/live-templates/javascript/expo.js`.

---
+++ live-templates/javascript/expo.js
@@ -0,0 +1,18 @@
+export { $SYMBOLS$ } from "$FILE_LOCATION$";$END$
+
+/* Category:     JavaScript
+ * Abbreviation: impo
+ * Description:  Inserts an `export { x } from "y";` statement.
+ * 
+ * Template Variable Settings:
+ * 
+ *   $FILE_LOCATION$
+ *     Expression: <none>
+ *     Default value: <none>
+ *     Skip is defined: NO
+ * 
+ *   $SYMBOLS$
+ *     Expression: completeSmart()
+ *     Default value: <none>
+ *     Skip is defined: NO
+ */
