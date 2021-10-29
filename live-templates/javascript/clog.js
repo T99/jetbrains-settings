0fe5090a6384d97228eb7739d2fa06b11440c405
Trevor Sears <trevor@trevorsears.com>
2021-10-29 13:56:39 -0400

    Added `/live-templates/javascript/clog.js`.

---
+++ live-templates/javascript/clog.js
@@ -0,0 +1,13 @@
+console.log($MSG$);$END$
+
+/* Category:     JavaScript
+ * Abbreviation: clog
+ * Description:  Inserts a 'console.log(...)' statement.
+ * 
+ * Template Variable Settings:
+ * 
+ *   $MSG$
+ *     Expression: <none>
+ *     Default value: "`Hello, world!`"
+ *     Skip is defined: NO
+ */
