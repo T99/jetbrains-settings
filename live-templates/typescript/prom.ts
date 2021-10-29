8557c00130de34cc03dd503ff970dd98d6ecc084
Trevor Sears <trevor@trevorsears.com>
2021-10-29 13:48:29 -0400

    Added `/live-templates/typescript/prom.ts`.

---
+++ live-templates/typescript/prom.ts
@@ -0,0 +1,18 @@
+new Promise<$TYPE$>((resolve: (value: $TYPE$) => void, reject: (reason?: $ERR_TYPE$) => void): void => {
+	
+	$END$
+	
+});
+
+/* Template Variable Settings:
+ * 
+ * $TYPE$
+ *   Expression: <none>
+ *   Default value: <none>
+ *   Skip is defined: NO
+  
+ * $ERR_TYPE$
+ *   Expression: <none>
+ *   Default value: "any"
+ *   Skip is defined: NO 
+ */
