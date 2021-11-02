0873c9cff4dc561c2d6859beade5eb972920ab05
Trevor Sears <trevor@trevorsears.com>
2021-11-02 11:55:11 -0400

    Added `/live-templates/typescript/eafm.ts`.

---
+++ live-templates/typescript/eafm.ts
@@ -0,0 +1,17 @@
+export async function main(): Promise<void> {
+	
+	$END$
+	
+}
+
+main().catch(console.error);
+  
+/* Category:     TypeScript
+ * Abbreviation: eafm
+ * Description:  Inserts a 'main' block.
+ * 
+ * Template Variable Settings:
+ * 
+ *   <none>
+ */
+  
